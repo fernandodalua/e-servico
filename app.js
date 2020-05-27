9066a1bd531096d49936a86c5aa49105d49ca74c
fernandodalua <dalua_fernando@hotmail.com>
2020-05-26 23:00:15 -0300

    Orçamento

--- app.js
+++ app.js
@@ -187,7 +187,7 @@ app.get('/orcamentos', (request, response) => {
 			response.send('Erro: ' + error)
 		}else{
 			cliente = results
-			query = "select id_cliente, DATE_FORMAT(data, '%d %m %Y') as data from orcamento where id_conta = '"+id_conta+"'"
+			query = "select c.nome, DATE_FORMAT(o.data, '%d/%m/%Y') from orcamento o, cliente c where o.id_cliente = c.id_cliente and o.id_conta = '"+id_conta+"'"
 			db.query(query, (error, results) => {
 				if(error){
 					response.send('Erro: ' + error)
