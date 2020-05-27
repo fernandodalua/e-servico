4d1078285eb9b96c05c2e0af5891a62fa8f75abb
fernandodalua <dalua_fernando@hotmail.com>
2020-05-27 15:40:57 -0300

    Orçamento

--- app.js
+++ app.js
@@ -267,6 +267,21 @@ app.post('/orcamento-add',[], function(request, response){
 })
 
 app.get('/orcamento-print', (request, response) => {
+	let id_orcamento = request.session.id_orcamento
+	let query = "select o.data, i.qtd, i.unitario, i.total, m.nome as nome_produto, c.nome as nome_cliente from orcamento o "
+	+"inner join orcamento_item i on o.id_orcamento = i.id_orcamento "
+	+"inner join cliente c on o.id_cliente = c.id_cliente"
+	+"inner join material m on i.id_material = m.id_material"
+	+"where o.id_orcamento = '"+id_orcamento+"'"
+	db.query(query, (error, results) => {
+		if(error){
+			response.send('Erro: ' + error)
+		}else{		
+			response.render('pages/print', {orcamento: results, layout: "print"})
+		}
+	})
+
+
 	response.render('pages/print', {layout: "print"})
 })
 
